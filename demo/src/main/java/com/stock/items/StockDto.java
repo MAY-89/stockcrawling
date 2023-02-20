06799d8c97772988a07e19f137967f64beda54c2
MAY-89 <hello.git.galic12@naver.com>
2023-02-21 00:48:02 +0900

    Dto 생성

---
+++ demo/src/main/java/com/stock/items/StockDto.java
@@ -0,0 +1,659 @@
+package com.stock.items;
+
+public class StockDto {
+  
+  private String index;
+  private String pe;
+  private String eps;
+  private String insiderOwn;
+  private String shsOutstand;
+  private String perfWeek;
+  private String marketCap;
+  private String fowardPe;
+  private String epsNextY;
+  private String iniderTrans;
+  private String shsFloat;
+  private String perfMonth;
+  private String income;
+  private String peg;
+  private String epsNextQ;
+  private String instOwn;
+  private String shortFloat;
+  private String perfQuarter;
+  private String sales;
+  private String ps;
+  private String epsThisY;
+  private String instTrans;
+  private String shortInterest;
+  private String perfHalfY;
+  private String bookSh;
+  private String pb;
+  private String epsNextYRatio;
+
+ 
+
+  private String roa;
+  private String targetPrice;
+  private String perfYear;
+  private String cashSh;
+  private String pc;
+  private String epsNextFiveY;
+  private String roe;
+  private String fiveTwoWRange;
+  private String perfYTD;
+  private String dividend;
+  private String pFCF;
+  private String epsPastFiveY;
+  private String roi;
+  private String fiveTowHight;
+  private String beta;
+  private String dividendRatio;
+  private String quickRatio;
+  private String salesPastFiveY;
+  private String grossMargine;
+  private String fiveTowLow;
+  private String atr;
+  private String employees;
+  private String currentRatio;
+  private String salesQQ;
+  private String operMargine;
+  private String rsi;
+  private String volatility;
+  private String optionable;
+  private String debtEq;
+  private String epsQQ;
+  private String profitMargin;
+  private String relVolume;
+  private String prevClose;
+  private String shortable;
+  private String ltDebtEq;
+  private String earnings;
+  private String payout;
+  private String avgVolume;
+  private String price;
+  private String recom;
+  private String sma20;
+  private String sma50;
+  private String sma200;
+  private String volume;
+  private String change;
+
+  public String getIndex() {
+    return this.index;
+  }
+
+  public void setIndex(String index) {
+    this.index = index;
+  }
+
+  public String getPe() {
+    return this.pe;
+  }
+
+  public void setPe(String pe) {
+    this.pe = pe;
+  }
+
+  public String getEps() {
+    return this.eps;
+  }
+
+  public void setEps(String eps) {
+    this.eps = eps;
+  }
+
+  public String getInsiderOwn() {
+    return this.insiderOwn;
+  }
+
+  public void setInsiderOwn(String insiderOwn) {
+    this.insiderOwn = insiderOwn;
+  }
+
+  public String getShsOutstand() {
+    return this.shsOutstand;
+  }
+
+  public void setShsOutstand(String shsOutstand) {
+    this.shsOutstand = shsOutstand;
+  }
+
+  public String getPerfWeek() {
+    return this.perfWeek;
+  }
+
+  public void setPerfWeek(String perfWeek) {
+    this.perfWeek = perfWeek;
+  }
+
+  public String getMarketCap() {
+    return this.marketCap;
+  }
+
+  public void setMarketCap(String marketCap) {
+    this.marketCap = marketCap;
+  }
+
+  public String getFowardPe() {
+    return this.fowardPe;
+  }
+
+  public void setFowardPe(String fowardPe) {
+    this.fowardPe = fowardPe;
+  }
+
+  public String getEpsNextY() {
+    return this.epsNextY;
+  }
+
+  public void setEpsNextY(String epsNextY) {
+    this.epsNextY = epsNextY;
+  }
+  public String getEpsNextYRatio() {
+    return this.epsNextYRatio;
+  }
+
+  public void setEpsNextYRatio(String epsNextYRatio) {
+    this.epsNextYRatio = epsNextYRatio;
+  }
+
+  public String getIniderTrans() {
+    return this.iniderTrans;
+  }
+
+  public void setIniderTrans(String iniderTrans) {
+    this.iniderTrans = iniderTrans;
+  }
+
+  public String getShsFloat() {
+    return this.shsFloat;
+  }
+
+  public void setShsFloat(String shsFloat) {
+    this.shsFloat = shsFloat;
+  }
+
+  public String getPerfMonth() {
+    return this.perfMonth;
+  }
+
+  public void setPerfMonth(String perfMonth) {
+    this.perfMonth = perfMonth;
+  }
+
+  public String getIncome() {
+    return this.income;
+  }
+
+  public void setIncome(String income) {
+    this.income = income;
+  }
+
+  public String getPeg() {
+    return this.peg;
+  }
+
+  public void setPeg(String peg) {
+    this.peg = peg;
+  }
+
+  public String getEpsNextQ() {
+    return this.epsNextQ;
+  }
+
+  public void setEpsNextQ(String epsNextQ) {
+    this.epsNextQ = epsNextQ;
+  }
+
+  public String getInstOwn() {
+    return this.instOwn;
+  }
+
+  public void setInstOwn(String instOwn) {
+    this.instOwn = instOwn;
+  }
+
+  public String getShortFloat() {
+    return this.shortFloat;
+  }
+
+  public void setShortFloat(String shortFloat) {
+    this.shortFloat = shortFloat;
+  }
+
+  public String getPerfQuarter() {
+    return this.perfQuarter;
+  }
+
+  public void setPerfQuarter(String perfQuarter) {
+    this.perfQuarter = perfQuarter;
+  }
+
+  public String getSales() {
+    return this.sales;
+  }
+
+  public void setSales(String sales) {
+    this.sales = sales;
+  }
+
+  public String getPs() {
+    return this.ps;
+  }
+
+  public void setPs(String ps) {
+    this.ps = ps;
+  }
+
+  public String getEpsThisY() {
+    return this.epsThisY;
+  }
+
+  public void setEpsThisY(String epsThisY) {
+    this.epsThisY = epsThisY;
+  }
+
+  public String getInstTrans() {
+    return this.instTrans;
+  }
+
+  public void setInstTrans(String instTrans) {
+    this.instTrans = instTrans;
+  }
+
+  public String getShortInterest() {
+    return this.shortInterest;
+  }
+
+  public void setShortInterest(String shortInterest) {
+    this.shortInterest = shortInterest;
+  }
+
+  public String getPerfHalfY() {
+    return this.perfHalfY;
+  }
+
+  public void setPerfHalfY(String perfHalfY) {
+    this.perfHalfY = perfHalfY;
+  }
+
+  public String getBookSh() {
+    return this.bookSh;
+  }
+
+  public void setBookSh(String bookSh) {
+    this.bookSh = bookSh;
+  }
+
+  public String getPb() {
+    return this.pb;
+  }
+
+  public void setPb(String pb) {
+    this.pb = pb;
+  }
+
+  public String getRoa() {
+    return this.roa;
+  }
+
+  public void setRoa(String roa) {
+    this.roa = roa;
+  }
+
+  public String getTargetPrice() {
+    return this.targetPrice;
+  }
+
+  public void setTargetPrice(String targetPrice) {
+    this.targetPrice = targetPrice;
+  }
+
+  public String getPerfYear() {
+    return this.perfYear;
+  }
+
+  public void setPerfYear(String perfYear) {
+    this.perfYear = perfYear;
+  }
+
+  public String getCashSh() {
+    return this.cashSh;
+  }
+
+  public void setCashSh(String cashSh) {
+    this.cashSh = cashSh;
+  }
+
+  public String getPc() {
+    return this.pc;
+  }
+
+  public void setPc(String pc) {
+    this.pc = pc;
+  }
+
+  public String getEpsNextFiveY() {
+    return this.epsNextFiveY;
+  }
+
+  public void setEpsNextFiveY(String epsNextFiveY) {
+    this.epsNextFiveY = epsNextFiveY;
+  }
+
+  public String getRoe() {
+    return this.roe;
+  }
+
+  public void setRoe(String roe) {
+    this.roe = roe;
+  }
+
+  public String getFiveTwoWRange() {
+    return this.fiveTwoWRange;
+  }
+
+  public void setFiveTwoWRange(String fiveTwoWRange) {
+    this.fiveTwoWRange = fiveTwoWRange;
+  }
+
+  public String getPerfYTD() {
+    return this.perfYTD;
+  }
+
+  public void setPerfYTD(String perfYTD) {
+    this.perfYTD = perfYTD;
+  }
+
+  public String getDividend() {
+    return this.dividend;
+  }
+
+  public void setDividend(String dividend) {
+    this.dividend = dividend;
+  }
+
+  public String getPFCF() {
+    return this.pFCF;
+  }
+
+  public void setPFCF(String pFCF) {
+    this.pFCF = pFCF;
+  }
+
+  public String getEpsPastFiveY() {
+    return this.epsPastFiveY;
+  }
+
+  public void setEpsPastFiveY(String epsPastFiveY) {
+    this.epsPastFiveY = epsPastFiveY;
+  }
+
+  public String getRoi() {
+    return this.roi;
+  }
+
+  public void setRoi(String roi) {
+    this.roi = roi;
+  }
+
+  public String getFiveTowHight() {
+    return this.fiveTowHight;
+  }
+
+  public void setFiveTowHight(String fiveTowHight) {
+    this.fiveTowHight = fiveTowHight;
+  }
+
+  public String getBeta() {
+    return this.beta;
+  }
+
+  public void setBeta(String beta) {
+    this.beta = beta;
+  }
+
+  public String getDividendRatio() {
+    return this.dividendRatio;
+  }
+
+  public void setDividendRatio(String dividendRatio) {
+    this.dividendRatio = dividendRatio;
+  }
+
+  public String getQuickRatio() {
+    return this.quickRatio;
+  }
+
+  public void setQuickRatio(String quickRatio) {
+    this.quickRatio = quickRatio;
+  }
+
+  public String getSalesPastFiveY() {
+    return this.salesPastFiveY;
+  }
+
+  public void setSalesPastFiveY(String salesPastFiveY) {
+    this.salesPastFiveY = salesPastFiveY;
+  }
+
+  public String getGrossMargine() {
+    return this.grossMargine;
+  }
+
+  public void setGrossMargine(String grossMargine) {
+    this.grossMargine = grossMargine;
+  }
+
+  public String getFiveTowLow() {
+    return this.fiveTowLow;
+  }
+
+  public void setFiveTowLow(String fiveTowLow) {
+    this.fiveTowLow = fiveTowLow;
+  }
+
+  public String getAtr() {
+    return this.atr;
+  }
+
+  public void setAtr(String atr) {
+    this.atr = atr;
+  }
+
+  public String getEmployees() {
+    return this.employees;
+  }
+
+  public void setEmployees(String employees) {
+    this.employees = employees;
+  }
+
+  public String getCurrentRatio() {
+    return this.currentRatio;
+  }
+
+  public void setCurrentRatio(String currentRatio) {
+    this.currentRatio = currentRatio;
+  }
+
+  public String getSalesQQ() {
+    return this.salesQQ;
+  }
+
+  public void setSalesQQ(String salesQQ) {
+    this.salesQQ = salesQQ;
+  }
+
+  public String getOperMargine() {
+    return this.operMargine;
+  }
+
+  public void setOperMargine(String operMargine) {
+    this.operMargine = operMargine;
+  }
+
+  public String getRsi() {
+    return this.rsi;
+  }
+
+  public void setRsi(String rsi) {
+    this.rsi = rsi;
+  }
+
+  public String getVolatility() {
+    return this.volatility;
+  }
+
+  public void setVolatility(String volatility) {
+    this.volatility = volatility;
+  }
+
+  public String getOptionable() {
+    return this.optionable;
+  }
+
+  public void setOptionable(String optionable) {
+    this.optionable = optionable;
+  }
+
+  public String getDebtEq() {
+    return this.debtEq;
+  }
+
+  public void setDebtEq(String debtEq) {
+    this.debtEq = debtEq;
+  }
+
+  public String getEpsQQ() {
+    return this.epsQQ;
+  }
+
+  public void setEpsQQ(String epsQQ) {
+    this.epsQQ = epsQQ;
+  }
+
+  public String getProfitMargin() {
+    return this.profitMargin;
+  }
+
+  public void setProfitMargin(String profitMargin) {
+    this.profitMargin = profitMargin;
+  }
+
+  public String getRelVolume() {
+    return this.relVolume;
+  }
+
+  public void setRelVolume(String relVolume) {
+    this.relVolume = relVolume;
+  }
+
+  public String getPrevClose() {
+    return this.prevClose;
+  }
+
+  public void setPrevClose(String prevClose) {
+    this.prevClose = prevClose;
+  }
+
+  public String getShortable() {
+    return this.shortable;
+  }
+
+  public void setShortable(String shortable) {
+    this.shortable = shortable;
+  }
+
+  public String getLtDebtEq() {
+    return this.ltDebtEq;
+  }
+
+  public void setLtDebtEq(String ltDebtEq) {
+    this.ltDebtEq = ltDebtEq;
+  }
+
+  public String getEarnings() {
+    return this.earnings;
+  }
+
+  public void setEarnings(String earnings) {
+    this.earnings = earnings;
+  }
+
+  public String getPayout() {
+    return this.payout;
+  }
+
+  public void setPayout(String payout) {
+    this.payout = payout;
+  }
+
+  public String getAvgVolume() {
+    return this.avgVolume;
+  }
+
+  public void setAvgVolume(String avgVolume) {
+    this.avgVolume = avgVolume;
+  }
+
+  public String getPrice() {
+    return this.price;
+  }
+
+  public void setPrice(String price) {
+    this.price = price;
+  }
+
+  public String getRecom() {
+    return this.recom;
+  }
+
+  public void setRecom(String recom) {
+    this.recom = recom;
+  }
+
+  public String getSma20() {
+    return this.sma20;
+  }
+
+  public void setSma20(String sma20) {
+    this.sma20 = sma20;
+  }
+
+  public String getSma50() {
+    return this.sma50;
+  }
+
+  public void setSma50(String sma50) {
+    this.sma50 = sma50;
+  }
+
+  public String getSma200() {
+    return this.sma200;
+  }
+
+  public void setSma200(String sma200) {
+    this.sma200 = sma200;
+  }
+
+  public String getVolume() {
+    return this.volume;
+  }
+
+  public void setVolume(String volume) {
+    this.volume = volume;
+  }
+
+  public String getChange() {
+    return this.change;
+  }
+
+  public void setChange(String change) {
+    this.change = change;
+  }
+  
+
+
+
+}

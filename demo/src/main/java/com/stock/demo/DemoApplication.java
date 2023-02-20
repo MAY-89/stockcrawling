15c94e74069f59def06e15ebf00356e7b390689a
MAY-89 <hello.git.galic12@naver.com>
2023-02-21 00:48:23 +0900

    throws 추가

--- demo/src/main/java/com/stock/demo/DemoApplication.java
+++ demo/src/main/java/com/stock/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.stock.demo;
 
 import java.io.IOException;
 
+import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +11,7 @@ import com.stock.crawling.Crawling;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException {
 		Crawling.getStockPriceList();
 		SpringApplication.run(DemoApplication.class, args);
 	}
